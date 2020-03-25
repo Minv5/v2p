23d339a34e5e042c3f5942e750f11544004385af
nzw9314 <iwi35zw6vt@privaterelay.appleid.com>
2020-03-25 14:29:55 +0800

    更新[快手极速版] cookie、task二合一

--- Script/kuaishou.js
+++ Script/kuaishou.js
@@ -4,44 +4,48 @@
 1.将下方[rewrite_local]和[MITM]地址复制的相应的区域
 下，
 2.APP登陆账号后，点击'红包',即可获取Cookie.
-
 仅测试Quantumult x，Surge、Loon自行测试
 by Macsuny
 感谢
 @Chavy
 @Nobyda
-
 ~~~~~~~~~~~~~~~~
 Surge 4.0 :
 [Script]
 cron "0 9 * * *" script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/kuaishou_sign.js
 # 获取快手极速版 Cookie.
-http-request https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview,script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/kuaishou_cookie.js
+http-request https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview,script-path=https://raw.githubusercontent.com/Sunert/Scripts/master/Task/kuaishou_sign.js
 ~~~~~~~~~~~~~~~~
 QX 1.0.5 :
 [task_local]
 0 9 * * * kuaishou_sign.js
-
 [rewrite_local]
-# 获取快手极速版 Cookie. QX 1.0.5(188+):
-https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview url script-request-header kuaishou_cookie.js
+# Get bilibili cookie. QX 1.0.5(188+):
+https:\/\/nebula\.kuaishou\.com\/rest\/n\/nebula\/activity\/earn\/overview url script-request-header kuaishou_sign.js
 ~~~~~~~~~~~~~~~~
 QX or Surge MITM = nebula.kuaishou.com
 ~~~~~~~~~~~~~~~~
-
 */
-const CookieName = '快手'
-const CookieKey = 'cookie_ks'
+const CookieName = '快手极速版'
+const cookieKey = 'cookie_ks'
 const sy = init()
-GetCookie();
+const cookieVal = sy.getdata(cookieKey);
+
+let isGetCookie = typeof $request !== 'undefined'
+
+if (isGetCookie) {
+   GetCookie()
+} else {
+   sign()
+}
 
 function GetCookie() {
   if ($request.headers) {
     var CookieValue = $request.headers['Cookie'];
     
-    if (sy.getdata(CookieKey) != (undefined || null)) {
-      if (sy.getdata(CookieKey) != CookieValue) {
-        var cookie = sy.setdata(CookieValue, CookieKey);
+    if (sy.getdata(cookieKey) != (undefined || null)) {
+      if (sy.getdata(cookieKey) != CookieValue) {
+        var cookie = sy.setdata(CookieValue, cookieKey);
         if (!cookie) {
           sy.msg("更新" + CookieName + "Cookie失败‼️", "", "");
           sy.log(`[${CookieName}] 获取Cookie: 失败`);
@@ -61,7 +65,57 @@ function GetCookie() {
   } else {
     sy.msg("写入" + CookieName + "Cookie失败‼️", "", "配置错误, 无法读取请求头, ");
   }
+sy.done
 }
+
+function sign() {
+      let detail = ``
+      let subTitle = ``
+	 let signurl = {
+		url: 'https://nebula.kuaishou.com/rest/n/nebula/sign/sign',
+		headers: {
+			Cookie: cookieVal
+		}
+	}
+    sy.get(signurl, (error, response, data) => {
+      sy.log(`${CookieName}, data: ${data}`)
+      let result = JSON.parse(data)
+      if(result.result == 10007){
+        subTitle = `签到结果: ${result.error_msg}`
+        sy.msg(CookieName,subTitle,'')
+       }
+          sy.done()
+     })
+	let earnurl = {
+		url: 'https://nebula.kuaishou.com/rest/n/nebula/sign/query',
+		headers: {
+			Cookie: cookieVal
+		}
+	}
+    sy.get(earnurl, (error, response, data) => {
+      sy.log(`${CookieName}, data: ${data}`)
+      let result = JSON.parse(data)
+     if (result.data.nebulaSignInPopup.button == '立即签到'){ 
+       subTitle = `签到成功: ${result.data.nebulaSignInPopup.subTitle}, ${result.data.nebulaSignInPopup.title}`
+      } else if (result.data.nebulaSignInPopup.button == '好的'){ 
+       subTitle = `重复签到: ${result.data.nebulaSignInPopup.subTitle}, ${result.data.nebulaSignInPopup.title}`
+      }
+    })
+    let reurl = {url:'https://nebula.kuaishou.com/rest/n/nebula/activity/earn/overview',
+    headers: {Cookie:cookieVal}
+   }
+	sy.get(reurl, (error, response, data) =>{
+	sy.log(`${CookieName}, data: ${data}`)
+	let result = JSON.parse(data) 
+	if (result.result == 1) {
+	        detail = `现金收益: 💵${result.data.allCash}元    金币收益: 💰${result.data.totalCoin}`
+			sy.msg(CookieName,subTitle,detail)	
+			} 
+          sy.log(`错误代码: ${result.result}, 返回信息: ${result.error_msg}`)
+	    })
+      }
+sy.done()
+      
 function init() {
   isSurge = () => {
     return undefined === this.$httpClient ? false : true
