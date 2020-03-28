3dd1a34673598c8c5531f65577b7536619d67e61
nzw9314 <iwi35zw6vt@privaterelay.appleid.com>
2020-03-28 17:54:09 +0800

    更新[电视预告] 增加现在时间

--- Task/tvpreview.js
+++ Task/tvpreview.js
@@ -18,13 +18,28 @@ var c = "cctv1"  // 可更改电视台，从电视家网络活动中获取，央
 var wurl = {
     url: "http://api.cntv.cn/epg/epginfo?serviceId=cbox&c="+c,
 };
+  var d = new Date()
+  var M = d.getMonth()+1
+  var D = d.getDate()
+  var h = ("0" + (d.getHours())).slice(-2)            
+  var m = ("0" + (d.getMinutes())).slice(-2)
+  var weekday=new Array(7);
+      weekday[0]="星期日";
+      weekday[1]="星期一";
+      weekday[2]="星期二";
+      weekday[3]="星期三";
+      weekday[4]="星期四";
+      weekday[5]="星期五";
+      weekday[6]="星期六";
+ var n = weekday[d.getDay()]
+
    $task.fetch(wurl).then(response => {    
      try{ 
       let result = JSON.parse(response.body)
       var i = 0                          
-      const title = `${result[`${c}`].channelName}节目预告`
-      subTitle = `正在播出: ${result[`${c}`].isLive}`
-      detail = `${result[`${c}`].program[i].showTime} ${result[`${c}`].program[i].t}`
+      const title = `现在是`+ M +'月'+ D +'日' + n + h +':'+ m 
+      subTitle = `${result[`${c}`].channelName}频道 节目预告  ` 
+      detail = `正在播出: ${result[`${c}`].isLive}\n${result[`${c}`].program[i].showTime} ${result[`${c}`].program[i].t}`
       
       for (i = 1; i < result[`${c}`].program.length; i++){
       detail += `\n${result[`${c}`].program[i].showTime} ${result[`${c}`].program[i].t}`
@@ -35,4 +50,3 @@ var wurl = {
        }
  });
 $done()
-
