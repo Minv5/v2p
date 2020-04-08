409b9e85181b23be3f91438a2e004f291bfa075c
nzw9314 <iwi35zw6vt@privaterelay.appleid.com>
2020-04-08 12:25:04 +0800

    update 彩云天气

--- NobyDa/QuantumultX/File/ColorWeather.js
+++ NobyDa/QuantumultX/File/ColorWeather.js
@@ -2,9 +2,23 @@
 CaiYun Weather (ColorWeather) unlock Vip
 
 QX 1.0.0:
-^https:\/\/biz\.caiyunapp\.com\/v2\/user\?app_name\=weather url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/ColorWeather.js
+^https:\/\/biz\.caiyunapp\.com\/v2\/user\?app_name\=weather url script-response-body https://raw.githubusercontent.com/nzw9314/QuantumultX/master/NobyDa/QuantumultX/File/ColorWeather.js
 
 MitM = biz.caiyunapp.com
 */
 
-var _0xe0e0=["\x62\x6F\x64\x79","\x70\x61\x72\x73\x65","\x78\x79\x5F\x76\x69\x70\x5F\x65\x78\x70\x69\x72\x65","\x72\x65\x73\x75\x6C\x74","\x69\x73\x5F\x76\x69\x70","\x76\x69\x70\x5F\x65\x78\x70\x69\x72\x65\x64\x5F\x61\x74","\x69\x73\x5F\x78\x79\x5F\x76\x69\x70","\x73\x74\x72\x69\x6E\x67\x69\x66\x79"];var body=$response[_0xe0e0[0]];var obj=JSON[_0xe0e0[1]](body);obj[_0xe0e0[3]][_0xe0e0[2]]= 4096483190;obj[_0xe0e0[3]][_0xe0e0[4]]= true;obj[_0xe0e0[3]][_0xe0e0[5]]= 4096483190;obj[_0xe0e0[3]][_0xe0e0[6]]= true;body= JSON[_0xe0e0[7]](obj);$done(body)
+var body = $response.body;
+ var url = $request.url;
+ var obj = JSON.parse(body);
+ const 1 = '/v2/user';
+ if (url.indexOf(1) != -1) {
+  obj.is_vip = true;
+  obj.vip_expired_at = 9576796302; 
+  obj.is_xy_vip = true; 
+  obj.xy_vip_expire = 9576796302;
+  body = JSON.stringify(obj);
+}
+ $done({
+ body
+}
+);
