63ce059715b11625e23221294487c2f2d850a7d0
ZhJ <zhj@yamyamyam.net>
2020-06-22 10:46:44 +0800

    增加水果和宠物的分享码（已满五个） 增加jd宠汪汪、jd种豆得豆js

--- Task/jd_pet.js
+++ Task/jd_pet.js
@@ -95,6 +95,7 @@ var shareCodes = [ // 这个列表填入你要助力的好友的shareCode, 最�
     'MTAxODcxOTI2NTAwMDAwMDAyNjA4ODQyMQ==',
     'MTAxODc2NTEzMDAwMDAwMDAwNTUwNDUxMw==',
     'MTAxODc2NTEzOTAwMDAwMDAxODQ5MDg5NQ==',
+    'MTAxODcxOTI2NTAwMDAwMDAxOTQ3MjkzMw=='
 ]
 
 var petInfo = null;
