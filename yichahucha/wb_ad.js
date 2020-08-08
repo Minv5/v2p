000830e727d85ec1f6f9fe33dd45d935b4ec393b
nzw9314 <nzw9314@github.com>
2020-08-08 22:25:51 +0800

    同步更新、添加油猴转换器

--- yichahucha/wb_ad.js
+++ yichahucha/wb_ad.js
@@ -22,6 +22,7 @@ const path17 = "/statuses/friends_timeline";
 const path18 = "/!/photos/pic_recommend_status";
 const path19 = "/statuses/video_mixtimeline";
 const path20 = "/video/tiny_stream_video_list";
+const path21 = "/photo/info";
 
 const url = $request.url;
 let body = $response.body;
@@ -100,9 +101,13 @@ if (
 } else if (url.indexOf(path19) != -1) {
     let obj = JSON.parse(body);
     delete obj.expandable_view;
-    if(obj.hasOwnProperty('expandable_views'))
+    if (obj.hasOwnProperty('expandable_views'))
         delete obj.expandable_views;
     body = JSON.stringify(obj);
+} else if (url.indexOf(path21) != -1) {
+    if (body.indexOf("ad_params") != -1) {
+        body = JSON.stringify({});
+    }
 }
 
 $done({ body });
@@ -139,34 +144,55 @@ function filter_timeline_cards(cards) {
         let j = cards.length;
         while (j--) {
             let item = cards[j];
+            let card_type = item.card_type;
             let card_group = item.card_group;
-            if (card_group && card_group.length > 0) {
-                let i = card_group.length;
-                while (i--) {
-                    let card_group_item = card_group[i];
-                    let card_type = card_group_item.card_type;
-                    if (card_type) {
-                        if (card_type == 9) {
-                            if (is_timeline_ad(card_group_item.mblog)) card_group.splice(i, 1);
-                        } else if (card_type == 118 || card_type == 89) {
-                            card_group.splice(i, 1);
-                        } else if (card_type == 42) {
-                            if (card_group_item.desc == '\u53ef\u80fd\u611f\u5174\u8da3\u7684\u4eba') {
-                                cards.splice(j, 1);
-                                break;
-                            }
-                        }
-                    }
-                }
-            } else {
-                let card_type = item.card_type;
-                if (card_type && card_type == 9) {
-                    if (is_timeline_ad(item.mblog)) cards.splice(j, 1);
-                }
+            if (card_type && card_type == 9) {
+                if (is_timeline_ad(item.mblog)) cards.splice(j, 1);
+            }else if (card_type && card_type == 11) {
+                filter_top_search(card_group);
+            }else {
+                filter_card_group(card_group);
             }
         }
     }
     return cards;
+}
+
+function filter_card_group(card_group) {
+    if (card_group && card_group.length > 0) {
+        let i = card_group.length;
+        while (i--) {
+            let card_group_item = card_group[i];
+            let card_type = card_group_item.card_type;
+            if (card_type) {
+                if (card_type == 9) {
+                    if (is_timeline_ad(card_group_item.mblog)) card_group.splice(i, 1);
+                } else if (card_type == 118 || card_type == 89) {
+                    card_group.splice(i, 1);
+                } else if (card_type == 42) {
+                    if (card_group_item.desc == '\u53ef\u80fd\u611f\u5174\u8da3\u7684\u4eba') {
+                        cards.splice(j, 1);
+                        break;
+                    }
+                } else if (card_type == 17) {
+                    filter_top_search(card_group_item.group);
+                }
+            }
+        }
+    }
+}
+
+function filter_top_search(group) {
+    if (group && group.length > 0) {
+        let k = group.length;
+        while (k--) {
+            let group_item = group[k];
+            console.log(group_item);
+            if (group_item.hasOwnProperty("promotion")) {
+                group.splice(k, 1);
+            }
+        }
+    }
 }
 
 function is_timeline_ad(mblog) {
